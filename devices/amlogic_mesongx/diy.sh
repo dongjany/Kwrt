13acc716554f89cac1e0e176e9253be441298a2f
dongjany <3145255636@qq.com>
2026-04-29 16:54:27 +0800

    Update diy.sh

--- devices/amlogic_mesongx/diy.sh
+++ devices/amlogic_mesongx/diy.sh
@@ -3,16 +3,9 @@
 shopt -s extglob
 
 rm -rf package/kernel
-git_clone_path master https://github.com/coolsnowwolf/lede target/linux/generic/backport-6.1 target/linux/generic/hack-6.1 target/linux/generic/pending-6.1 target/linux/amlogic package/boot/uboot-amlogic package/kernel
-
-rm -rf package/kernel/bpf-headers
-git_clone_path openwrt-24.10 https://github.com/openwrt/openwrt package/kernel/bpf-headers
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/amlogic package/boot/uboot-amlogic package/kernel
 
 sed -i "s/autocore-arm/autocore/" target/linux/amlogic/Makefile
-
-wget -N https://github.com/coolsnowwolf/lede/raw/refs/heads/master/include/kernel-6.1 -P include/
-
-rm -rf feeds/routing/batman-adv package/kernel/rtw88-usb target/linux/generic/hack-6.1/983-add-bcm-fullconenat-to-nft.patch
 
 echo '
 CONFIG_BLK_DEV_INTEGRITY=n
